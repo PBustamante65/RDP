fd9730c1c30856d146309a1f18d6d86298dab144
PBustamante65 <136853530+PBustamante65@users.noreply.github.com>
2025-06-28 14:15:21 -0600

    Update

--- Programas/Act10.py
+++ Programas/Act10.py
@@ -2,19 +2,24 @@
 
 
 import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import RobustScaler
+from sklearn.preprocessing import PowerTransformer
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
+from imblearn.over_sampling import SMOTE
 
 
 class Bayesiano:
 
-    def __init__(self, df, num=True):
+    def __init__(self, df, num=True, priors=None):
         self.num = num
         self.df = df
+        self.priors = priors
 
     def load_df(self):
 
@@ -23,7 +28,20 @@ class Bayesiano:
         if self.num == True:
             df = df.select_dtypes(include=np.number)
 
-        return df
+        df_selected = df[df["Machine_Type"] == "Drill"]
+
+        # # Assuming df is your DataFrame
+        # df_1 = df[df["target"] == 1]
+        # df_0 = df[df["target"] == 2]
+        #
+        # # Downsample 1s to match number of 0s
+        # df_1_downsampled = df_1.sample(len(df_0), random_state=42)
+        #
+        # # Combine downsampled 1s with 0s
+        # df_balanced = pd.concat([df_1_downsampled, df_0]).sample(
+        #     frac=1, random_state=42
+        # )  # shuffle
+        return df_selected
 
     def load_data(self):
 
@@ -105,15 +123,33 @@ class Bayesiano:
             X, y, test_size=0.2, random_state=42
         )
 
-        scaler = StandardScaler()
+        print(X_train.shape, y_train.shape)
+
+        # scaler = StandardScaler()
+        # scaler = MinMaxScaler()
+        # scaler = RobustScaler()
+        scaler = PowerTransformer(method="yeo-johnson")
         X_train_scaled = scaler.fit_transform(X_train)
         X_test_scaled = scaler.transform(X_test)
+        # X_train_scaled = X_train
+        # X_test_scaled = X_test
 
         if data.ndim == 1:
             data = data.reshape(1, -1)
         data_scaled = scaler.transform(data)
+        # Create data_scaled and force it to be a numeric data type
+        # data_scaled = data.astype(np.float64)
+        print(data_scaled)
 
-        gnb = GaussianNB()
+        sm = SMOTE(random_state=42)
+        over_X, over_y = sm.fit_resample(X_train_scaled, y_train)
+
+        X_train_scaled = over_X
+        y_train = over_y
+
+        gnb = (
+            GaussianNB(priors=self.priors) if self.priors is not None else GaussianNB()
+        )
         gnb.fit(X_train_scaled, y_train)
 
         y_pred = gnb.predict(X_test_scaled)
@@ -138,7 +174,7 @@ class Bayesiano:
                 print(f"{features[j]}: {dato[j]}")
 
             print("\n")
-            print(f"Prediccion: {classes[int(prediccion) -1]}\n")
+            print(f"Prediccion: {classes[prediccion]}\n")
             print(f"Probabilidades: {dict(zip(classes, probabilidad))}\n")
 
         groups = np.unique(y)
@@ -192,7 +228,18 @@ class Bayesiano:
                 plt.show()
 
 
-df = "/Users/patrickbustamante/Library/CloudStorage/GoogleDrive-p317694@uach.mx/My Drive/Verano Reconocimiento/Bayesian/CreditData.csv"
+#
+# df = pd.read_csv(
+#     "/Users/patrickbustamante/Library/CloudStorage/GoogleDrive-p317694@uach.mx/My Drive/Verano Reconocimiento/Bayesian/machine_failure_dataset.csv"
+# )
+# print(df.head())
+# print(df["Machine_Type"].value_counts())
+# df = df[df["Machine_Type"] == "Mill"]
+# print(df["Failure_Risk"].value_counts())
+
+df = "/Users/patrickbustamante/Library/CloudStorage/GoogleDrive-p317694@uach.mx/My Drive/Verano Reconocimiento/Bayesian/machine_failure_dataset.csv"
+# df = "/Users/patrickbustamante/Library/CloudStorage/GoogleDrive-p317694@uach.mx/My Drive/Verano Reconocimiento/Bayesian/CreditData.csv"
 # df = "/Users/patrickbustamante/Library/CloudStorage/GoogleDrive-p317694@uach.mx/My Drive/Verano Reconocimiento/Bayesian/data.csv"
-Bayes = Bayesiano(df)
+# priors = [0.72, 0.28]
+Bayes = Bayesiano(df, num=False)
 Bayes.classify()
